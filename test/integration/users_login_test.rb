74af5c5806915a40718e52900269dcfd4677bcf0
Deep-Red <kirbycolebrown@gmail.com>
2018-05-26 04:30:51 -0400

    Testing setup. Login test.

---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:one)
+  end
+
+  test "login with valid information" do
+    get new_user_session_path
+    post user_session_path, params: { user: { email:  @user.email, password: '123greetings' } }
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_response :success
+    assert_template 'layouts/application'
+    assert_template 'queued_episodes/index'
+    assert_select "aside", {count: 1, text: "Signed in successfully."}
+  end
+
+  test "reject login with invalid information" do
+    get new_user_session_path
+    post user_session_path, params: { user: { email: @user.email, password: 'wrong' } }
+    assert_response :success
+    assert_template 'layouts/application'
+    assert_select "aside", { count: 1, text: "Invalid Email or password." }
+  end
+
+end
