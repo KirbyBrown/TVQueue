1e2d0aecd6d9852270db58852abb62a744c44c0e
Deep-Red <kirbycolebrown@gmail.com>
2018-05-27 05:35:24 -0400

    User model validations and tests. Fixes #26.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  before_save { email.downcase! }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: { case_insensitive: false }
+
   has_many :queued_episodes, foreign_key: "user_id", dependent: :destroy
   has_many :episodes, through: :queued_episodes
 end
