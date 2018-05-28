e7d5b57bbae700d61e516e98afb537ff8b952506
Deep-Red <kirbycolebrown@gmail.com>
2018-05-27 22:33:59 -0400

    Integration testing for login.

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -17,9 +17,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "aside", {count: 1, text: "Signed in successfully."}
   end
 
-  test "reject login with invalid information" do
+  test "reject login with wrong password" do
     get new_user_session_path
     post user_session_path, params: { user: { email: @user.email, password: 'wrong' } }
+    assert_response :success
+    assert_template 'layouts/application'
+    assert_select "aside", { count: 1, text: "Invalid Email or password." }
+  end
+
+  test "reject login with wrong username" do
+    get new_user_session_path
+    post user_session_path, params: { user: { email: "bademail@example.com", password: @user.password } }
     assert_response :success
     assert_template 'layouts/application'
     assert_select "aside", { count: 1, text: "Invalid Email or password." }
