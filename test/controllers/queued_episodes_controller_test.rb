dd8109f9bef9c0673ff061c0e390f294e04fc8a0
Deep-Red <kirbycolebrown@gmail.com>
2018-05-28 03:11:01 -0400

    First queued episodes controller tests.

--- test/controllers/queued_episodes_controller_test.rb
+++ test/controllers/queued_episodes_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class QueuedEpisodesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:one)
+    sign_in(@user, '123greetings')
+  end
+
+  test "should get index" do
+    get queued_episodes_index_path
+    assert_response :success
+  end
+
 end
