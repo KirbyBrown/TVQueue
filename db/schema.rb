b7b8a65aaae17e1cef26571c4cdcde1c27d05ecf
Deep-Red <kirbycolebrown@gmail.com>
2018-07-15 01:04:16 -0400

    Email confirmation mailer and tests.

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 20180714074122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_stat_statements"
 
   create_table "episodes", force: :cascade do |t|
     t.bigint "tv_show_id"
