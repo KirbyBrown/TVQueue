eb6ac4fa526a1f6749252d0e30277402cdaa276c
Deep-Red <kirbycolebrown@gmail.com>
2018-07-21 21:33:26 -0400

    Set 'from' email address for password resets.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = 'b4c73ea5890d271646df2aa4800b614d17c3acffcd79df949496ee18e135d0539d0910220a6aff2245096b8a6e9edb09a1867e544beaa3b2955896b70fc9bf08'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'donotreply@tvq.ninja'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
