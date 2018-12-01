6e4574ddae30fb1eee9629cd0845f98f98b35e68
Deep-Red <kirbycolebrown@gmail.com>
2018-12-01 09:31:11 -0500

    last_seen_at for User.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :set_last_seen_at, if: proc { current_user && (current_user.last_seen_at.nil? || current_user.last_seen_at < 15.minutes.ago) }
+
+  private
+  def set_last_seen_at
+    current_user.update_column(:last_seen_at, Time.now)
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180714074122) do
+ActiveRecord::Schema.define(version: 20181201141951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20180714074122) do
     t.string "confirmation_digest"
     t.boolean "confirmed", default: false
     t.datetime "confirmed_at"
+    t.datetime "last_seen_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
