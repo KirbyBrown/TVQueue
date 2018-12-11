1f433b01d030a3fdce2c16da31750af4fcc82fc4
Deep-Red <kirbycolebrown@gmail.com>
2018-12-10 20:43:51 -0500

    Corrected typo.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
-    :enable_starttls_auto => true
+    :enable_starttls_auto => true,
     :from           => 'Password Reset'
   }
 
