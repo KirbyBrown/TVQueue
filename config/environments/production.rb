fffa39b02c48c65df41cdc8132264d46bfe5f4c3
Deep-Red <kirbycolebrown@gmail.com>
2018-12-10 20:40:18 -0500

    Adjustments to sendgrid configuration - relevant to password reset requests.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
-  host = 'tvq.ninja'
+  host = 'https://www.tvq.ninja'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
@@ -76,6 +76,7 @@ Rails.application.configure do
     :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
+    :from           => 'Password Reset'
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
